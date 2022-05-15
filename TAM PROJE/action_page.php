0eb9f86972bfe26c1c51de67a6945f889eeec198
sloggg <104869273+sloggg@users.noreply.github.com>
2022-05-15 17:54:57 +0300

    Add files via upload

---
+++ TAM PROJE/action_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="stil.css">
+    <meta charset="utf-8">
+    
+    
+    
+    <title></title>
+   
+    
+</head>
+<body>
+<?php
+    echo $_POST['adsoyad'];
+    echo $_POST['id'];
+    echo $_POST['psw'];
+    echo $_POST['email'];
+    echo $_POST['cinsiyet'];
+?>
+</body>
+</html>
